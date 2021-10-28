be267eeb9de9f653f87ffea63445d04622fc932f
Felipe <felhenao@gmail.com>
2021-10-28 11:26:14 -0400

    Refactor App.js: - Move data file import to Grid.js - Convert from class based component to functional component - Move state to GridItem.js - Remove updateClicked function - Remove props passed to child component (Grid.js) - Apply ESLint Code Style Rules

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import styles from './App.module.css';
 import Title from './Title/Title';
 import Background from './Background/Background';
@@ -6,31 +6,14 @@ import Grid from './Grid/Grid';
 import Rocket from './Rocket/Rocket';
 import Smoke from './Rocket/Smoke';
 
-// Data
-import facts from './data/NASA_facts.json';
+const App = () => (
+  <div className={styles.App}>
+    <Title />
+    <Background />
+    <Grid />
+    <Rocket />
+    <Smoke />
+  </div>
+);
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { facts };
-  }
-
-  updateClicked(id) {
-    const stateCopy = Object.assign({}, this.state);
-    stateCopy.facts[id].clicked = true;
-    console.log(stateCopy);
-    this.setState(stateCopy);
-  }
-  
-  render() {
-    return (
-      <div className={styles.App}>
-        <Title />
-        <Background />
-        <Grid facts={this.state.facts} updateClicked={this.updateClicked.bind(this)} />
-        <Rocket />
-        <Smoke />
-      </div>
-    );
-  }
-}
+export default App;
