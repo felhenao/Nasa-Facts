f56939980af3f3615382b6b7ff286c969e916c3f
Felipe <felhenao@gmail.com>
2021-10-29 11:33:33 -0400

    Switch Title to fragments

--- src/Title/Title.js
+++ src/Title/Title.js
@@ -2,9 +2,9 @@ import React from 'react';
 import styles from './Title.module.css';
 
 const Title = () => (
-  <div>
+  <>
     <h1 className={styles.title}>NASA Facts</h1>
-  </div>
+  </>
 );
 
 export default Title;
