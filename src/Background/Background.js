c99c94d661387b880f0c0413eab82e27cb66e13a
Felipe <felhenao@gmail.com>
2021-10-29 11:27:05 -0400

    Switch Background and Rocket to fragments

--- src/Background/Background.js
+++ src/Background/Background.js
@@ -2,11 +2,11 @@ import React from 'react';
 import styles from './Background.module.css';
 
 const Background = () => (
-  <div>
+  <>
     <div className={styles.stars} />
     <div className={styles.alpha} />
     <div className={styles.nebula} />
-  </div>
+  </>
 );
 
 export default Background;
--- src/Rocket/Rocket.js
+++ src/Rocket/Rocket.js
@@ -2,9 +2,9 @@ import React from 'react';
 import styles from './Rocket.module.css';
 
 const Rocket = () => (
-  <div>
+  <>
     <img src="/shuttle.png" alt="shuttle" className={styles.img} />
-  </div>
+  </>
 );
 
 export default Rocket;
