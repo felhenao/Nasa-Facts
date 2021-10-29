146fab790c0132a09cb35fd661812e8294d13fda
Felipe <felhenao@gmail.com>
2021-10-29 17:16:21 -0400

    Title: Switch to styled-components

--- src/Title/Title.js
+++ src/Title/Title.js
@@ -1,10 +1,28 @@
 import React from 'react';
-import styles from './Title.module.css';
+import styled from 'styled-components';
+
+const MainTitle = styled.h1`
+  font-family: 'Roboto', san-serif;
+  font-size: 4em;
+  color: white;
+  position: fixed;
+  left: 50%;
+  transform: translate(-50%, 0);
+  width: 100%;
+  margin-bottom: 50px;
+  background-color: rgba(0, 0, 0, 0.5);
+  margin-top: 0; 
+  margin-bottom: 0;
+  padding-top: 0.67em;
+  padding-bottom: 0.67em;
+
+  @media (max-width: 768px) {
+    font-size: 3em;
+  }
+`;
 
 const Title = () => (
-  <>
-    <h1 className={styles.title}>NASA Facts</h1>
-  </>
+  <MainTitle>NASA Facts</MainTitle>
 );
 
 export default Title;
