54bbbe5f09ce1742265fd7a4b10469a6550f82e1
Felipe <felhenao@gmail.com>
2021-10-29 12:24:07 -0400

    Refactor GridItem component - Destructure fact prop - Add state using useState hook to toggle between Explorer and Badge components (images) on click - Add onClick event listener to GridItem card - Remove updateClicked prop received from parent component (Grid)

--- src/Grid/GridItem.js
+++ src/Grid/GridItem.js
@@ -1,17 +1,23 @@
-import React from 'react';
+/* eslint-disable */
+import React, { useState } from 'react';
 import styles from './GridItem.module.css';
 import Badge from './Badge';
 import Explorer from './Explorer';
 
-const GridItem = (props) => {
-  return (
-    <div className={styles.card} onClick={() => props.updateClicked(props.fact.id)}>
-      {props.fact.clicked ? <Explorer /> : <Badge />}
-      <h2 className={styles.tytle}>{props.fact.title}</h2>
-      <article className={styles.fact}>{props.fact.fact}</article>
-    </div>
-  )
+const GridItem = ({ fact }) => {
+  const [clicked, setclicked] = useState(fact.clicked);
 
-}
+  return (
+    <div
+      role="article"
+      className={styles.card}
+      onClick={() => setclicked(!clicked)}
+    >
+      {clicked ? <Explorer /> : <Badge />}
+      <h2 className={styles.title}>{fact.title}</h2>
+      <article className={styles.fact}>{fact.fact}</article>
+    </div>
+  );
+};
 
 export default GridItem;
