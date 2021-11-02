4a1988c5e30e6469d2f2e0baa3a5f10300a4dad6
Felipe <felhenao@gmail.com>
2021-11-02 12:52:54 -0400

    Background: Switch to styled-components

--- src/Background/Background.js
+++ src/Background/Background.js
@@ -1,11 +1,50 @@
 import React from 'react';
-import styles from './Background.module.css';
+import styled, { keyframes, css } from 'styled-components';
+import starsImg from '../Images/stars.png';
+import alphaImg from '../Images/alpha.png';
+import nebulaImg from '../Images/nebula.png';
+
+const backgroundItems = css`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  width: 100%;
+  height: 100%;
+  display: block;
+`;
+
+const starGazing = keyframes`
+  from { background-position: 0 0;}
+  to { background-position: -10000px 5000px;}
+`;
+
+const Stars = styled.div`
+  ${backgroundItems}
+  background: black url(${starsImg}) repeat top center;
+  z-index: -3;
+`;
+
+const Alpha = styled.div`
+  ${backgroundItems}
+  background: transparent url(${alphaImg}) repeat top center;
+  z-index: -2;
+  animation: ${starGazing} 500s linear infinite;
+`;
+
+const Nebula = styled.div`
+  ${backgroundItems}
+  background: transparent url(${nebulaImg}) repeat top center;
+  z-index: -1;
+  opacity: 0.15;
+`;
 
 const Background = () => (
   <>
-    <div className={styles.stars} />
-    <div className={styles.alpha} />
-    <div className={styles.nebula} />
+    <Stars />
+    <Alpha />
+    <Nebula />
   </>
 );
 
